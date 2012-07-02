4aa60e034c16fd27b012f9c4ae06ced707178626
Anthony Johnston <aj@antix.co.uk>
2012-07-02 19:54:55 +0100

    using semantic versioning

--- testing/Testing/Properties/AssemblyInfo.cs
+++ testing/Testing/Properties/AssemblyInfo.cs
@@ -35,6 +35,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.1.*")]
-[assembly: AssemblyInformationalVersion("1.1-dev")]
+[assembly: AssemblyVersion("1.1.0.*")]
+[assembly: AssemblyInformationalVersion("1.1.0-1")]
 [assembly: AssemblyFileVersion("1.1.0.0")]
