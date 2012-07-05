d43d8e5944b3f104aba56f2ed397e001dd2566bc
Anthony Johnston <aj@antix.co.uk>
2012-07-05 23:37:23 +0100

    update version

--- testing/Testing/Properties/AssemblyInfo.cs
+++ testing/Testing/Properties/AssemblyInfo.cs
@@ -36,5 +36,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("2.0.0.*")]
-[assembly: AssemblyInformationalVersion("2.0.0-b01")]
+[assembly: AssemblyInformationalVersion("2.0.0-b02")]
 [assembly: AssemblyFileVersion("2.0.0.0")]
